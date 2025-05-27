31b7466987e764a0919c38ba61ea979c2b89b830
FlameAndFlash <h2wiens@uwaterloo.ca>
2025-05-26 21:08:41 -0400

    Finished todo list: Lesson on css

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -11,13 +11,13 @@ function renderTodoList() {
   for(let i = 0; i < todoList.length; i++){
     const todoObject = todoList[i];
     const { name, dueDate } = todoObject;
-    const html = `<p>
-      ${name} ${dueDate} 
+    const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
         <button onclick="
           todoList.splice(${i}, 1);
           renderTodoList();
-        ">Delete</button>
-      </p>`;
+        " class="delete-todo-button" >Delete</button>`;
     todoListHTML += html;
   }
 
